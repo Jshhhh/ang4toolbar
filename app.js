7dc185c80fe57b3d8403f06564539dd1de078e2a
Justin Shih <justinshih@Justins-MacBook-Pro.local>
2018-09-06 00:37:58 -0700

    Removes get request

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ const app = express();
 
 app.use('/', express.static(path.join(__dirname, 'dist/ang4-tooblar/')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/ang4-tooblar/index.html'));
-});
-
 const port = process.env.PORT || '3030';
 
 app.listen(port, () => console.log(`listening on ${port}`));
